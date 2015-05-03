508e999f1eb468dd986a9ca6207677bf2e5ed611
Yongeun <woang1892@gmail.com>
2015-05-03 05:50:44 +0000

    Add all project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyWay::Application.config.secret_key_base = 'd5db25cf8a4aabf0f6a3b308006de9f997a3e994c5a958f3ec6d85d9ff0e24fa2e17fcff8a59d8f6f80632aec480e86406c620528000698613ccd95fcf4ccf46'
